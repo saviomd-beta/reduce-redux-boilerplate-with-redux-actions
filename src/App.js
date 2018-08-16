73a0c55b0662d7fdf4d878a56bb6815ef95ea619
Sávio Mendes <saviomd@gmail.com>
2018-08-16 11:32:22 -0300

    Lesson 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+import Message from './components/Message'
+import Footer from './components/Footer'
+import Loader from './components/Loader'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Welcome to React with Redux</h2>
+        </div>
+        <Router>
+          <div className="Todo-App">
+            <Message />
+            <Loader />
+            <TodoForm />
+            <Route path='/:filter?' render={({match}) => (
+                <TodoList filter={match.params.filter} />
+              )} />
+            <Footer />
+          </div>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default App
